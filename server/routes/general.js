a31836d82345e438924f4515963bdea49ef7ddda
abdullahfikri <mfikri.dev@gmail.com>
2023-02-28 17:41:39 +0700

    Finishing Admin dashboard
    Add some feature in dashboard:
    -Dashboard
    -Product list,
    -Customers list,
    -Transactions list,
    -Geography for list country customers
    -Sales list
    -And More...

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,3 +1,6 @@
 import express from 'express';
+import { getUser, getDashboardStat } from '../controllers/general.js';
 const router = express.Router();
+router.get('/user/:id', getUser);
+router.get('/dashboard', getDashboardStat);
 export default router;
--- server/routes/management.js
+++ server/routes/management.js
@@ -1,3 +1,7 @@
+import { getAdmins, getUserPerformance } from '../controllers/management.js';
 import express from 'express';
 const router = express.Router();
+
+router.get('/admins', getAdmins);
+router.get('/performance/:id', getUserPerformance);
 export default router;
